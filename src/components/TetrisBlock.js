166c0c1b71a2467b8cba8a84312063579b702f92
Florian Heuberger <fh@flogersoft.de>
2024-08-23 15:42:09 +0200

    updated blocks

--- src/components/TetrisBlock.js
+++ src/components/TetrisBlock.js
@@ -1,10 +1,28 @@
-// src/components/TetrisBlock.js
 import React from "react";
 import "./TetrisBlock.module.scss";
-// src/components/TetrisBlock.js
+
+import red from "../assets/red.png";
+import blue from "../assets/blue.png";
+import green from "../assets/green.png";
+import yellow from "../assets/yellow.png";
+import lightblue from "../assets/lightblue.png";
+import orange from "../assets/orange.png";
+import violet from "../assets/violet.png";
+
+const blockImages = {
+  red,
+  blue,
+  green,
+  yellow,
+  lightblue,
+  orange,
+  violet,
+};
 
 const TetrisBlock = ({ color }) => {
-  const backgroundColor = color === 0 ? "#d3d3d3" : color; // Setze leere Zellen auf hellgrau
+  const backgroundColor = color === 0 ? "#d3d3d3" : "#d3d3d3"; // Setze leere Zellen auf hellgrau
+
+  const backgroundImage = color !== 0 ? `url(${blockImages[color]})` : "none"; // Verwende das Bild, wenn color nicht 0 ist
 
   return (
     <div
@@ -12,7 +30,13 @@ const TetrisBlock = ({ color }) => {
         width: "32px",
         height: "32px",
         backgroundColor: backgroundColor,
-        border: "1px solid #ccc",
+        border:
+          color === 0 || color === "white"
+            ? "1px solid #ccc"
+            : "0px solid #00000000",
+        backgroundImage: backgroundImage,
+        backgroundSize: "cover", // Skaliert das Bild, um den gesamten Block zu füllen
+        backgroundPosition: "center", // Zentriert das Bild
       }}
     ></div>
   );
