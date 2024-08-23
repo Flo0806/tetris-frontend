fb1e45cc256d9b5e7297904c56641016839b2332
Florian Heuberger <fh@flogersoft.de>
2024-08-23 12:15:13 +0200

    added tetrominos

---
+++ src/utils/tetrominos.js
@@ -0,0 +1,68 @@
+// src/tetrominos.js
+
+export const TETROMINOS = {
+  0: { shape: [[0]], color: "0" }, // Leeres Block-Array
+  I: {
+    shape: [
+      [0, "I", 0, 0],
+      [0, "I", 0, 0],
+      [0, "I", 0, 0],
+      [0, "I", 0, 0],
+    ],
+    color: "lightblue",
+  },
+  J: {
+    shape: [
+      [0, "J", 0],
+      [0, "J", 0],
+      ["J", "J", 0],
+    ],
+    color: "blue",
+  },
+  L: {
+    shape: [
+      [0, "L", 0],
+      [0, "L", 0],
+      [0, "L", "L"],
+    ],
+    color: "orange",
+  },
+  O: {
+    shape: [
+      ["O", "O"],
+      ["O", "O"],
+    ],
+    color: "yellow",
+  },
+  S: {
+    shape: [
+      [0, "S", "S"],
+      ["S", "S", 0],
+      [0, 0, 0],
+    ],
+    color: "green",
+  },
+  T: {
+    shape: [
+      [0, "T", 0],
+      ["T", "T", "T"],
+      [0, 0, 0],
+    ],
+    color: "violet",
+  },
+  Z: {
+    shape: [
+      ["Z", "Z", 0],
+      [0, "Z", "Z"],
+      [0, 0, 0],
+    ],
+    color: "red",
+  },
+};
+
+export const randomTetromino = () => {
+  const tetrominos = "IJLOSTZ";
+  const randTetromino =
+    tetrominos[Math.floor(Math.random() * tetrominos.length)];
+  return TETROMINOS[randTetromino];
+};
